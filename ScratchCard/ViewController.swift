d43219b30315d1a074568fd6578fd640014d37a5
Anand Nigam <anandnigam.2aug@gmail.com>
2021-07-22 17:23:55 +0530

    Initial Commit

---
+++ ScratchCard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScratchCard
+//
+//  Created by Anand Nigam on 22/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
